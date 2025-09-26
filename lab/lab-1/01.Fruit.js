5d9291ab918213db5a3d363005c7f15b36156d99
DELL-3\User <darinazhivkova>
2025-09-26 09:52:21 +0300

    Added task 2

---
+++ lab/lab-1/01.Fruit.js
@@ -0,0 +1,8 @@
+function solve(fruit, weight, price) {
+    weight = Number(weight) / 1000;
+    let money = Number(weight)/1000 * Number(price);
+    console.log(`I need $${money.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`);
+}
+
+solve('orange', 2500, 1.80);
+solve('apple', 1563, 2.35);
